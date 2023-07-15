80ecc9e9d4d7d203342dd67aa03ba0c284a1cd69
Ntuthuko <ntuthukolu@gmail.com>
2023-07-15 21:40:57 +0200

    base

--- models/base_model.py
+++ models/base_model.py
@@ -5,25 +5,37 @@
 
 import uuid
 from datetime import datetime
-
+import models
 
 class BaseModel:
     """
         A base class for all hbnb models
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
             Assign a unique ID to each instance using and converts to string
         """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key , value)
+            s = '%Y-%m-%dT%H:%M:%S.%f'
+            self.created_at = datetime.strptime(self.created_at, s)
+            self.updated_at = datetime.strptime(self.updated_at, s)
+        else:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
+        models.storage.new(self)
 
     def __str__(self):
+        """Returns string rep""" 
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
+        """updates instance"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
