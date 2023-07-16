1349d92d705b373b8082f6975ed56a6704c1bbc9
Tshupane Morake <40323964+Papase14@users.noreply.github.com>
2023-07-16 23:30:50 +0200

    state class

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    State Module for HBNB project
+"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+        State class
+    """
+    name = ""
