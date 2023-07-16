39732deeaed40a921031201d3cf6235d8e2b0384
Tshupane Morake <40323964+Papase14@users.noreply.github.com>
2023-07-16 23:31:59 +0200

    review class

---
+++ models/review.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    Review module for the HBNB project
+"""
+
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """
+        Review classto store review information
+    """
+    place_id = ""
+    user_id = ""
+    text = ""
